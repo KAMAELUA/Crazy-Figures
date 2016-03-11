f97b5aea34cf2469d892cce38d79c31c1a21caec
Andrey Chugay <kamaelua@gmail.com>
2016-03-11 12:46:32 +0200

    added randomizer

---
+++ Work Project 1 Figures/Randomizer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work_Project_1_Figures
+{
+    public static class Randomizer
+    {
+        public static int GetRandomArgbColor()
+        {
+            Random R = new Random();
+            int red = R.Next(0, 255);
+            int green = R.Next(0, 255);
+            int blue = R.Next(0, 255);
+
+            return Color.FromArgb(255, red, green, blue).ToArgb();
+        }
+    }
+}
